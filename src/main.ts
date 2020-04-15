f250a5a7c5ab6cc1a104850bfaf61b365c40ed1d
pytness <pytness@gmail.com>
2020-04-15 10:48:02 +0200

    Imported alert-toast

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import alertToast from './plugins/alert-toast';
+
+Vue.use(alertToast, { sampleConfig: 1 });
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
 }).$mount('#app')

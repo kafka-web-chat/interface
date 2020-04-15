86f573bcb2cead41841ee0ecbce43b359173c8f1
pytness <pytness@gmail.com>
2020-04-15 10:46:24 +0200

    Added alert-toast component

---
+++ src/plugins/alert-toast/alert-toast.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="at_container">
+	<v-row align="end"
+		justify="end">
+		<v-col cols="2">
+			<v-alert v-for="([i, data]) in $test.notifications"
+				:key="i"
+				:type="data.type"
+				text
+				dismissible
+				border="left"
+				@input="close(i)">
+				{{data.text}}
+			</v-alert>
+		</v-col>
+	</v-row>
+	<!-- <v-alert v-for="(message, i) in $test.messages">{{message}}</v-alert> -->
+</div>
+</template>
+<script lang="ts">
+import {
+	Component,
+	Vue
+} from 'vue-property-decorator';
+
+@Component({})
+export default class AlertToast extends Vue {
+	mounted() {
+		this.$test.linkedComponents.push(this);
+	}
+
+	close(key: number) {
+		this.$test.delete(key)
+	}
+}
+</script>
+<style>
+.at_container {
+	width: 100%;
+
+	position: absolute;
+	bottom: 0px;
+}
+</style>

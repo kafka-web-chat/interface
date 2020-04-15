670558fbcf6708ac2868963dbba4462982f5ce41
pytness <pytness@gmail.com>
2020-04-15 10:25:21 +0200

    Added some sample store states

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		connectedToBackend: false,
+		connectedUsers: [
+			{ name: 'Pedro', icon: 'tune-vertical' },
+			{ name: 'Alvaro', icon: 'package-variant-closed' },
+			{ name: 'Jesus', icon: 'palette-outline' },
+			{ name: 'Mario', icon: 'plus' }, // github-circle loading?? idk wth is this
+		]
+	}
 })

7a3c3bb74432bd1bf827909e2a66536213c018e7
pytness <pytness@gmail.com>
2020-04-15 10:34:30 +0200

    Added routes to the views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,25 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+import Connect from '@/views/Connect.vue'
+import Login from '@/views/Login.vue'
+import Chat from '@/views/Chat.vue'
+
+
+
+const routes: Array<RouteConfig> = [
+	// { path: '/', redirect: '/login' },
+	{ path: '/connect', component: Connect },
+	{ path: '/login', component: Login },
+	{ path: '/chat', component: Chat },
+	{ path: '/*', redirect: '/' }
 ]
 
 const router = new VueRouter({
-  routes
+	// mode: 'abstract',
+	routes
 })
 
-export default router
+export default router;
